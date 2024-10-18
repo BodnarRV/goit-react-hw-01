619d787a065f5a4c347e137537385bebf6a7f679
Rostyslav Bodnar <85370164+BodnarRV@users.noreply.github.com>
2024-10-18 15:49:37 +0300

    1st homewirk React

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./FriendList.css";
+import FriendListItem from "./FriendListItem/FriendListItem";
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className="friend-list">
+      {friends.map((friend) => (
+        <li key={friend.id} className="friend-list-item">
+          <FriendListItem 
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
